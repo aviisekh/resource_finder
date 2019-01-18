1fc9223d09883b3aa108fce85fb5f6d8431b747c
Abhishek <aviisekh@gmail.com>
2019-01-18 14:42:47 +0545

    completed

--- person.rb
+++ person.rb
@@ -10,7 +10,7 @@ require 'pry'
 MINUTE_STEP = 30 # minutes steps considered for the available slots
 TOTAL_HOURS = 24 # Total hours in a day
 MINUTES_IN_HOUR = 60 # Minutes in one hour
-TOTAL_MINUTES = 1440  #Total minutes in a day
+TOTAL_MINUTES = 1440 # Total minutes in a day
 
 
 # Set up a database that resides in RAM
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define do
     t.string :name
     t.string :start_time
     t.string :end_time
+    t.text :t_r_vector
   end
 end
 
@@ -36,54 +37,63 @@ end
 #   self.abstract_class = true
 # end
 
+# String Class
 class String
-    def minutize
-        time_arr = self.split(':').map{ |x| x.to_i}
-        time_arr[0]*60 + time_arr[1]
-    end
+  def minutize
+    time_arr = split(':').map{ |x| x.to_i}
+    time_arr[0] * 60 + time_arr[1]
+  end
 end
 
+# Range Vector Class
 class RangeVector
-    attr_reader :vector
-    
-    def initialize(start_time='0:0', end_time='0:0')
-        @vector = rangify(start_time, end_time)
+  attr_reader :vector
+
+  def initialize(start_time='0:0', end_time='0:0')
+    @vector = rangify(start_time, end_time)
+  end
+
+  def rangify(start_time, end_time)
+    range_vector = []
+    (MINUTE_STEP..TOTAL_MINUTES).step(MINUTE_STEP) do |x|
+      range_vector << ((x > start_time.minutize && x <= end_time.minutize) ? 1 : 0)
     end
-    
-    def rangify(start_time, end_time)
-        range_vector = []
-        (MINUTE_STEP..TOTAL_MINUTES).step(MINUTE_STEP) do |x|
-            range_vector<< ((x>start_time.minutize && x<=end_time.minutize) ? 1 : 0)
-        end
-        range_vector
-    end
+    range_vector
+  end
 end
 
-
+# Person Class
 class Person < ActiveRecord::Base
-    def time_range_vector
-        time_range_vector_size = (MINUTES_IN_HOUR/MINUTE_STEP)*TOTAL_HOURS
-        vr = RangeVector.new
-        
-        available_time_ranges = Person.where(name: self.name).pluck(:start_time,:end_time)
-        time_range_availability_vector = Array.new(time_range_vector_size,0)
-        
-        available_time_ranges.each do |at|
-            range_vector = vr.rangify(at[0], at[1])
-            range_vector.each_with_index do |element,index|
-                was_previously_slot_available = !time_range_availability_vector[index].zero?
-                is_slot_available = !range_vector[index].zero?
-                time_range_availability_vector[index] = (was_previously_slot_available || is_slot_available) ? 1 : 0 
-            end
-        end
-        
-        time_range_availability_vector
+  serialize :t_r_vector
+  
+  def time_range_vector
+    time_range_vector_size = (MINUTES_IN_HOUR/MINUTE_STEP)*TOTAL_HOURS
+    vr = RangeVector.new
+
+    available_time_ranges = Person.where(name: self.name).pluck(:start_time,:end_time)
+    time_range_availability_vector = Array.new(time_range_vector_size,0)
+
+    available_time_ranges.each do |at|
+      range_vector = vr.rangify(at[0], at[1])
+      range_vector.each_with_index do |element,index|
+        was_previously_slot_available = !time_range_availability_vector[index].zero?
+        is_slot_available = !range_vector[index].zero?
+        time_range_availability_vector[index] = (was_previously_slot_available || is_slot_available) ? 1 : 0 
+      end
     end
 
-    def is_available_for?(start_time, end_time)
-        to_check_range_vector = RangeVector.new(start_time, end_time).vector
-        available_range_vector = self.time_range_vector
+    time_range_availability_vector
+  end
+
+  def available_for?(start_time, end_time)
+    to_check_range_vector = RangeVector.new(start_time, end_time).vector
+    time_range_vector.each_with_index do |availability, index|
+      return false if (availability - to_check_range_vector[index]) < 0
     end
+    return true
+  end
+
+  def store_time_range_vector
+    self.update!(t_r_vector: time_range_vector)
+  end
 end
-
-
--- play.rb
+++ play.rb
@@ -6,11 +6,30 @@ Person.create!(name:'Ram', start_time: '15:00', end_time: '20:45')
 Person.create!(name:'Shyam', start_time: '12:00', end_time: '18:30')
 Person.create!(name:'Hari', start_time: '08:00', end_time: '23:59')
 
-
-p =  Person.where(name:"Ram")
+p = Person.where(name:"Ram")
 trv = p.first.time_range_vector
+p.first.store_time_range_vector
+v = RangeVector.new('12:00', '18:30') 
+a = p.first.available_for?('1:00', '5:00')
+b = p.first.available_for?('6:00', '6:30')
+c = p.first.available_for?('15:30', '21:30')
 
-v = RangeVector.new('12:00', '18:30')
-binding.pry
 # v.rangify('12:00', '18:30')
 # p.first.time_range_vector
+
+def person_available_for(start_time, end_time)
+    available_people = []
+    Person.select(:name).distinct.each do |person|
+        available_people << person if person.available_for?(start_time, end_time) 
+    end
+
+    return available_people
+end
+
+message = "Available Persons are "
+
+person_available_for("15:30","18:00").each do |person|
+    message += person.name + ","
+end
+
+puts message
